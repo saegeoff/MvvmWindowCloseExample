1d8df0b64c39ea43fd5f018e30e3249ea8f1198d
Geoffrey Trees <gptrees@gmail.com>
2016-07-01 15:06:36 -0400

    initial

---
+++ MvvmWindowCloseExample/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MvvmWindowCloseExample.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MvvmWindowCloseExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            LibMatlConcreteView vw = new LibMatlConcreteView();
+            vw.Show();
+        }
+    }
+}
